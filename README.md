306d731560cd6bc0be93c4b5e995c9f15afb0c54
E22CSEU1553 <asheeshgaur06@gmail.com>
2025-06-25 09:02:08 +0530

    react project Building

---
+++ README.md
@@ -0,0 +1,34 @@
+<!-- git init
+git branch -M main
+git add.
+git commit -m "react episode"
+git remote add origin git@github.com:namastedev(username)/namaste-react(repository-name).git
+git push origin main
+
+ -->
+ <!-- npm init
+ /npm init -y
+ test command:jest
+npm install -D parcel
+.gitignore
+npx parcel index.html(starting our app)
+npm install react
+npm install react-dom
+import  React from "react"
+import ReactDOM from "react-dom/client"
+<script type="module" src="./script.js"></script>
+npx parcel build index.html
+"browser-list":[
+last 2 versions
+]
+
+
+
+  -->
+JSX is not html in js
+it is a html like or Xml like syntax
+react element is an object
+const jsxheading=<h1>Namaste React Using JSX</h1>;
+
+ # Namaste React
+ 
---
+++ script.js
@@ -0,0 +1,24 @@
+
+// <div id=parent>
+//      <div id=child>
+//           <h1>hey what's up></h1>
+//           <h2>hey what's up</h2>
+//      </div>
+//      <div id=child2>
+//           <h1>hey what's up></h1>
+//           <h2>hey what's up</h2>
+//      </div>
+// </div>
+
+
+// const parent=React.ceateElement("div",{id:parent},
+//     [React.createElement("div",{id:child},
+//     [React.createElement("h1",{},"hey what's up h1"),
+//     React.createElement("h2",{},"hey what's up h2")]),
+//     React.createElement("div",{id:child2},
+//     [React.createElement("h1",{},"hey what's up h1"),
+//     React.createElement("h2",{},"hey what's up h2")])]
+// );
+// console.log(parent);
+// const root=ReactDOM.ceateRoot(document.getElementById("box"));
+// root.render(parent);
